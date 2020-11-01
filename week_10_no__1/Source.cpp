60c516ab87561087092cc2b822d97d533fd3dc17
pawida sattayamuk <TangTana@hotmail.com>
2020-11-01 17:30:29 +0700

    1

--- week_10_no__1/Source.cpp
+++ week_10_no__1/Source.cpp
@@ -13,34 +13,54 @@ int main() {
 	int num;
 	scanf("%d", &num);
 	int digit = 65;
+	int digit1 = 90;
 
 	if (num % 2 == 0) {
 		for (int i = 1; i <= num; i++) {
+
 			int m = digit;
+
 			for (int j = 1; j <= num; j++) {
+				
 				if (m == 91) {
 					m = 65;
 				}
+				
 				build( i, j, m, num, digit);
 				m++;
 			}
+
 			printf("\n");
 			digit += 1;
+
+			if (digit == 91) {
+				digit = 65;
+			}
+		
 		}
 	}
 
 	if (num % 2 != 0) {
 		for (int i = 1; i <= num; i++) {
-			int m = digit + 25;
+			
+			int m = digit1;
+			
 			for (int j = 1; j <= num; j++) {
+				
 				if (m == 64) {
 					m = 90;
 				}
-				build(i, j, m, num, digit);
+				
+				build(i, j, m, num, digit1);
 				m--;
 			}
+			
 			printf("\n");
-			digit -= 1;
+			digit1 -= 1;
+
+			if (digit1 == 64) {
+				digit1 = 90;
+			}
 		}
 	}
 
